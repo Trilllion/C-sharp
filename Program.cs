a2529435005ddcf3a23d5a00747e4ec1e7930f3a
Trilllion <raj.karan3102@gmail.com>
2016-12-15 22:37:35 +0530

    Multiples of 3 and 5
    Find the sum of all the multiples of 3 or 5 below 1000.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿    using System;
+
+class multiply
+{
+    public static void Main()
+    {
+        number();
+        Console.ReadLine();
+
+    }
+
+    static void number()
+    {
+        int k, y, t, div, sum = 0;
+        string kstring, ystring;
+        Console.WriteLine("Please enter number 1 and 2");
+        kstring = Console.ReadLine();
+        k = int.Parse(kstring);
+        ystring = Console.ReadLine();
+        y = int.Parse(ystring);
+        for (t = 0; t < 1000; t++)
+        {
+            div = t % k;
+
+            if ((t % 5 == 0) || (t % 3 == 0))
+            {
+                sum = sum + t;
+            }
+
+        }
+            Console.WriteLine("The sum of multiple of " + k + " and " + y + " is : " + sum);
+
+
+
+
+        
+    }
+}
