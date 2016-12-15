08936e918e9dc86d1d1a3d374f4ed6ffd7c27e1d
Trilllion <raj.karan3102@gmail.com>
2016-12-16 00:02:14 +0530

    C#

---
+++ Program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class multiply
+{
+    public static void Main()
+    {
+        long sum = 0, t1 = 1, t = 1, SUM = 0;
+        Console.WriteLine("Please enter to start");
+        while(t < 4000000)
+        {
+            sum = t + t1;
+            t1 = t;
+            t = sum;
+            if (t%2 == 0)
+            {
+                SUM = t + SUM;
+            }
+        }
+        Console.WriteLine(SUM);
+        Console.ReadLine();
+    }   
+}
